047e91bccc99cfd517a3414b52edad6ea05fbc55
Manuela Garcia Yebra <manuelagarciayebra@gmail.com>
2022-10-26 13:13:18 +0200

    first conditionals

---
+++ index.py
@@ -0,0 +1,19 @@
+user_option = input('¿Rock, paper o scissers? => ')
+computer_option = 'paper'
+
+if user_option == computer_option:
+    print('tie!')
+elif user_option == 'rock':
+    if computer_option == 'scissers'
+    print('rock beats scissor')
+    print('congrats you win!')
+    else: 
+        print('paper beats scissers')
+        print('soz computer wins :(')
+elif user_option == 'scissers':
+    if computer_option == 'paper':
+        print('scissers beats paper')
+        prin('congrats you win!')
+    else:
+        print('scissor beats paper')
+        print('soz computer wins :(')
